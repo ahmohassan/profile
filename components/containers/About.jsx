124deb6450ca070ce8d612562fc60b03da7197a1
ahmed <som2018mali@gmail.com>
2022-11-04 11:13:57 +0300

    contact aaded w git and phone

--- components/containers/About.jsx
+++ components/containers/About.jsx
@@ -113,32 +113,31 @@ const About = () => {
       }
     },[inView])
   return (
-    <section  class="flex  my-20    md:my-36 p-2  flex-col md:flex-row items-center justify-center ">
-        <motion.div className='md:w-screen' whileHover={{}} whileInView={{opacity :[0,1.9]}}transitionTo={{duration :1 ,type:'tween',ease:'easeInOut'}}>
+    <section  class="flex  my-20    md:my-36 p-2  flex-wrap md:flex-row items-center justify-center ">
+        <motion.div className='' whileHover={{}} whileInView={{opacity :[0,1.9]}}transitionTo={{duration :1 ,type:'tween',ease:'easeInOut'}}>
             <img src={image} className="  " alt="" srcset="" />
         </motion.div>
-        <div className=" flex-col justify-start mr-4 items-start  ">
+        <div className="flex-1 flex-col justify-start w-5/6 m-4 items-start  ">
             <motion.div whileInView={{y:-20, transition:{duration:1, type:"keyframes" ,bounce:0.9}}} >
-                <h1 className='text-[42px] md:text-[54px] font-bold'>What we doing in our customers?</h1>
+                <h1 className='text-[24px] md:text-[42px] mb-3 font-bold'>Solving customer problems gives developers a morale boost</h1>
                 <motion.div whileInView={{y:-10, transition:{duration:2.5, type:"keyframes" ,bounce:0.9}}}>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur repellendus atque possimus commodi quod dolorem pariatur iusto optio, rem molestias eos libero necessitatibus ex fuga perspiciatis delectus. Optio consequatur sed tempore officia porro!
-                        Natus voluptas ea sed minima ratione minus sit suscipit eum nihil in cupiditate velit officiis reiciendis odio quos a rem repellat neque quia omnis, soluta architecto temporibus! Corrupti, atque magnam.
+                      At times, it can be frustrating having to deal with customers. However, many developers report getting a boost in morale when dealing with end users. They can see close up how customers are using their products and get real feedback from those who use products on a daily basis. Seeing how their work is being used gives them motivation to ‘do better’. 
                     </p>
                 </motion.div>
             </motion.div>
             <motion.div whileInView={{y:-25, transition:{duration:3, type:"keyframes" ,bounce:0.9}}} className="flex flex-col md:flex-row p-4  md:justify-around  md:p-5 rounded-lg mt-10  items-start  shadow-2xl shadow-indigo-500 bg-gradient-to-l  from-gray-900 to-indigo-800/5">
                 <div className='flex flex-col '>
                     <div className="flex flex-row items-center ">
-                        <img src="/favicon.ico" className='w-8 mr-2' alt="" />
+                        <img src="/certification.png" className='w-8 mr-2' alt="" />
                         <span className='text-2xl font-mono font-bold'>Cerfied</span>
                     </div>   
 
                      {/*  */}
                      <div className='flex flex-col   pr-5 '>
-                        <h1 className='text-[16px] md:text-[14px] my-2 font-bold'>What we doing in our customers?</h1>
-                        <p className='text-[8px] md:text-lg mr-4'>
-                            Lorem ipsum dolor sit amet consectetur repellendus atque  velit officiis reiciendis odio quos a rem repellat neque quia omnis, soluta architecto temporibus! Corrupti, atque magnam.
+                        {/* <h1 className='text-[16px] md:text-[14px] my-2 font-bold'>What we doing in our customers?</h1> */}
+                        <p className='text-[8px] mt-4 md:text-lg mr-4'>
+                        Providing direct customer support gives developers the opportunity to understand their products as customers see them. As a product matures, developers can begin to feel disengaged from it in its entirety. That’s because developers often work in silos, focusing only on the features they’re building individually.
                         </p>
                     </div>
                 </div>
@@ -148,15 +147,14 @@ const About = () => {
                 <div className='flex-col '>
                     <div className="flex flex-row items-center my-2 ">
                         <div className='text-xl font-bold'>⭐⭐⭐⭐</div>
-                        <span className='text-2xl font-mono font-bold'>Cerfied </span>
+                        {/* <span className='text-2xl font-mono font-bold'>Cerfied </span> */}
                     </div>   
 
                      {/*  */}
                      <div className='flex flex-col  mt-2'>
-                        <h1 className='my-2 font-bold'>What we doing in our customers?</h1>
+                        {/* <h1 className='my-2 font-bold'>What we doing in our customers?</h1> */}
                         <p className='text-[8px] md:text-lg mr-4'>
-                            Lorem ipsum dolor repellendus atque  velit officiis reiciendis odio quos a rem repellat neque quia omnis, soluta architecto temporibus! Corrupti, atque magnam.
-                        </p>
+                        All software companies share the same aim; to provide the highest quality of service possible. And that goes further than just developing the best software. When it comes to providing the best customer service, companies need to implement the best technology, such as MDM systems.                        </p>
                     </div>
                 </div>
             </motion.div>
--- components/containers/contact.jsx
+++ components/containers/contact.jsx
@@ -13,27 +13,61 @@ const contact = () => {
       });
       e.target.reset()
   };
+  const contactData =[
+    {
+        image : '/phone.png',
+        url: 'tel:252617237048',
+        title :'Phone',
+        
+    },
+    {
+        image : '/github.png',
+        url: 'https://github.com/ahmohassan',
+        title :'GitHub',
+
+    },
+    {
+        image : '/whatsapp.png',
+        url: 'https://wa.link/czo3k2',
+        title :'WhatsApp',
+
+    }
+  ]
   const form = useRef();
   return (
-    <section className="bg-white bg-gradient-to-r from-gray-900 to-indigo-800">
-  <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
-      <form ref={form} method='post' onSubmit={sendEmail} action="#" className="space-y-8">
-          <div>
-              <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-              <input   name='email' type="emails" id="emails" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@email.com" required />
-          </div>
-          <div>
-              <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-              <input type="text" id="subject"  name='subject'   className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
-          </div>
-          <div className="sm:col-span-2">
-              <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-              <textarea id="message" rows="6"  name='message'  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
-          </div>
-              <button  className="bg-indigo-600 py-4 text-lg font-bold px-8 rounded-2xl my-1 hover:bg-white hover:text-indigo-600 hover:shadow-2xl">Send message</button>
-           </form>
+    <section className="flex flex-col items-center justify-center bg-gradient-to-r from-gray-900 to-indigo-800">
+        <h2 className="m-8 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+        {/* <div className='flx-col m-4  '> */}
+            <div className='flex flex-wrap justify-center md:flex-row items-center '>
+                {
+                    contactData.map((data)=>(
+                        <a href={data.url}>
+                        <div className='flex m-4 p-4 w-80 md:w-80 items-center flex-col text-center bg-primary rounded-xl  border-indigo-700/75  hover:bg-transparent/50 hover:shadow-2xl hover:shadow-indigo-500/80'>
+                            <img src={data.image} className='md:w-32 w-16 ' h alt="" />
+                            <p className='text-lg mt-4 font-bold'>{data.title}</p>
+                        </div>
+                        </a>
+                    ))
+                }
+            </div>
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      
+            <p className="mt-8 lg:m-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Please fill out the contact form below to get in touch with me, and I will do my best to respond to you as soon as I can</p>
+            <form ref={form} method='post' onSubmit={sendEmail} action="#" className="space-y-8">
+            <div>
+                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                <input   name='email' type="emails" id="emails" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@email.com" required />
+            </div>
+            <div>
+                <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                <input type="text" id="subject"  name='subject'   className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
+            </div>
+            <div className="sm:col-span-2">
+                <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                <textarea id="message" rows="6"  name='message'  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+            </div>
+                <button  className="bg-indigo-600 py-4 text-lg font-bold px-8 rounded-2xl my-1 hover:bg-white hover:text-indigo-600 hover:shadow-2xl">Send message</button>
+            </form>
         </div>
     </section>
   )
